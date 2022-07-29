30d4afa5dcbcc70f028dfafa24bbfbc5b33148f4
Vlad Galayko <lorddarkvlad@gmail.com>
2022-07-29 20:15:58 +0300

    add error component

---
+++ src/components/errorMassage/ErrorMassage.js
@@ -0,0 +1,9 @@
+import img from '../errorMassage/error.gif';
+
+const ErrorMassage = () => {
+    return (
+        <img style={{display: 'block', width: "250px", height: "250px", objectFit: 'contain', margin: "0 auto"}} src={img} alt="Error" />
+    )
+}
+
+export default ErrorMassage;

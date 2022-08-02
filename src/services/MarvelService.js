cdd707b6468bb72413be343465e1850c33d4c619
Vlad Galayko <lorddarkvlad@gmail.com>
2022-08-02 14:26:32 +0300

    add CharListLoaded

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=8f06c3025a9f6588ab47fb1ed29b558d'
+    _baseOffset = 210
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -11,8 +13,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
     getCharacter = async (id) => {
